02fb6592787d8a42eecc14048250c810df232e0f
Vinicius Eduardo <vinicius_eduardo19@outlook.com>
2025-04-20 18:37:27 -0300

    first commit

---
+++ src/test/java/dev/java/CadastroUsuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
