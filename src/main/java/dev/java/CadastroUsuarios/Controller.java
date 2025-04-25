113910bf3a52967effe8c378ca437d40192f6abf
Vinicius Eduardo <vinicius_eduardo19@outlook.com>
2025-04-25 11:03:01 -0300

    Criado a Controller

---
+++ src/main/java/dev/java/CadastroUsuarios/Controller.java
@@ -0,0 +1,13 @@
+package dev.java.CadastroUsuarios;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Bem-vindo ao sistema de cadastro de usuários!";
+    }
+}
