77373ab06e802a2197ff5ff2910332fe1d305d40
Vinicius Eduardo <vinicius_eduardo19@outlook.com>
2025-05-21 19:35:59 -0300

    feat: Atualiza o controller com o mock de dados

--- src/main/java/dev/java/CadastroUsuarios/Users/UserController.java
+++ src/main/java/dev/java/CadastroUsuarios/Users/UserController.java
@@ -1,7 +1,5 @@
 package dev.java.CadastroUsuarios.Users;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -10,4 +8,26 @@ public class UserController {
     public String boasVindas(){
         return "Bem-vindo ao sistema de cadastro de usuários!";
     }
+
+    // Adicionar Usuarios(CREATE)
+    @PostMapping("/add")
+    public String addUser(){
+        return "Usuario adicionado com sucesso!";
+    }
+    // Procurar Usuarios por ID(READ)
+    @GetMapping("/ShowId")
+    public String showId(){
+        return "Usuario encontrado com sucesso!";
+    }
+    // Alterar dados dos usuarios(UPDATE)
+    @PutMapping("/ChangeById")
+    public String changeById(){
+        return "Dados alterados com sucesso!";
+    }
+    // Deletar usuarios(DELETE)
+    @DeleteMapping("/DeleteId")
+    public String deleteById(){
+        return "Usuario deletado com sucesso!";
+    }
+    // 
 }
